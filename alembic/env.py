62e3ef647f36a491293cec16800a07572a0edc81
scmacoll <85879687+scmacoll@users.noreply.github.com>
2022-04-08 19:09:03 +1000

    fixed phone issue

--- alembic/env.py
+++ alembic/env.py
@@ -18,8 +18,7 @@ config.set_main_option(
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
